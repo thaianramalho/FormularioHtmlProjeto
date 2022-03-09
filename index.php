430a2672546a52f1bece96f5d18225f6a1d22a01
Thaian Ramalho <thaianramalho32@gmail.com>
2022-03-09 13:22:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ function calculoAnuncioPremium($custo, $notaFiscalPorcentagem, $premiumPorcentag
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styile.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5201865439212405"
      crossorigin="anonymous"></script>
      
