606fbfec0e63d55ac44005b225728bbf81df3e51
Thaian Ramalho <thaianramalho9@gmail.com>
2023-01-16 17:16:15 -0300

    nova calculadora

--- calculadora-ml.php
+++ calculadora-ml.php
@@ -2,54 +2,21 @@
 define('VALOR_LIMITE_TAXA', 79);
 define('TAXA_VALOR_INFERIOR', 5);
 define('TAXA_VALOR_SUPERIOR', 0);
-$quantidade = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['quantidade'] : 0;
-$produto = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['produto'] : 0;
-$notaFiscal = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['notaFiscal'] : 0;
-$frete = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['frete'] : 0;
-$despesas = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesas'] : 0;
-$classico = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['classico'] : 0;
-$premium = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['premium'] : 0;
-$venda = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['venda'] : 0;
-$custo = $quantidade * $produto;
-// variáveis criadas apenas para salvar os valores dos inputs e não resetar os valores dos inputs após clicar em submit
-$quantidadeR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['quantidade'] : "";
-$produtoR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['produto'] : "";
-$notaFiscalR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['notaFiscal'] : "";
-$freteR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['frete'] : "";
-$despesasR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesas'] : "";
-$classicoR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['classico'] : "";
-$premiumR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['premium'] : "";
-$vendaR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['venda'] : "";
 
-$calculoclassico = calculoAnuncioClassico($custo, porcentagem($notaFiscal,$venda), porcentagem($classico, $venda), $frete, $despesas, $venda);
-$totalclassico = number_format(($venda - $calculoclassico), 2);
+$despesasFixas = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesasFixas'] : 0;
+$despesasVariaveis = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesasVariaveis'] : 0;
+$margemLucro = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['margemLucro'] : 0;
+$custo = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['custo'] : 0;
 
-$calculopremium = calculoAnuncioPremium($custo , porcentagem($notaFiscal,$venda), porcentagem($premium, $venda), $frete, $despesas, $venda);
-$totalpremium = number_format(($venda - $calculopremium), 2);
+// fixar valores nos inputs
+$despesasFixasR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesasFixas'] : 0;
+$despesasVariaveisR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['despesasVariaveis'] : 0;
+$margemLucroR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['margemLucro'] : 0;
+$custoR = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST['custo'] : 0;
 
-// função de porcentagem (usada na nota fiscal)
-function porcentagem($porcentagem, $venda){
-    return ($porcentagem / 100) * $venda;
-}
+$custoTotal = 100 / (100 - ($despesasFixas + $despesasVariaveis + $margemLucro));
+$precoVenda = number_format($custo * $custoTotal, 2);
 
-// função que valida o limite do valor da taxa do mercado livre (a partir de 79 reais ele para de cobrar 5 reais de taxa)
-function validaLimiteDeTaxa($valorDaVenda)
-{
-    if ($valorDaVenda < VALOR_LIMITE_TAXA) {
-        return TAXA_VALOR_INFERIOR;
-    }
-    return TAXA_VALOR_SUPERIOR;
-}
-//calculando anuncio no plano clássico
-function calculoAnuncioClassico($custo, $notaFiscalPorcentagem, $classicoPorcentagem, $frete, $despesas, $venda)
-{
-    return $custo + $notaFiscalPorcentagem + $classicoPorcentagem + $frete + $despesas + validaLimiteDeTaxa($venda);
-}
-//calculando anuncio no plano premium
-function calculoAnuncioPremium($custo, $notaFiscalPorcentagem, $premiumPorcentagem, $frete, $despesas, $venda)
-{
-    return $custo + $notaFiscalPorcentagem + $premiumPorcentagem + $frete + $despesas + validaLimiteDeTaxa($venda);
-}
 ?>
 
 <!DOCTYPE html>
@@ -100,43 +67,28 @@ function calculoAnuncioPremium($custo, $notaFiscalPorcentagem, $premiumPorcentag
             <h2 class="text-center">Calculadora Mercado Livre</h2>
             <div class="illustration">
                 <i class="icon ion-ios-calculator" style="border-color: #5850fe;color: #5850fe;"></i></div>
-                <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" name="quantidade" id="quantidade" min="0" value="<?php echo $quantidadeR ?>" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;" min="1">
-                <label for="quantidade" class="labelInput">Quantidade</label></div>
             <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $produtoR ?>" name="produto" id="produto" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;" min="1">
-                <label for="produto" class="labelInput">Custo do produto (R$)</label></div>
+                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $custoR ?>" name="custo" id="custo" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;" min="1">
+                <label for="custo" class="labelInput">Custo do produto</label></div>
             <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $notaFiscalR ?>" name="notaFiscal" id="notaFiscal" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                <label for="notaFiscal" class="labelInput">Imposto da Nota Fiscal (%)</label></div>
+                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $despesasFixasR ?>" name="despesasFixas" id="despesasFixas" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
+                <label for="despesas" class="labelInput">Despesas Fixas</label></div>
             <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $despesasR ?>" name="despesas" id="despesas" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                <label for="despesas" class="labelInput">Despesas de venda (R$)</label></div>
+                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $despesasVariaveisR ?>" name="despesasVariaveis" id="despesasVariaveis" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
+                <label for="despesas" class="labelInput">Despesas Variáveis</label></div>
             <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $freteR ?>" name="frete" id="frete" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                <label for="frete" class="labelInput">Valor do frete (R$)</label></div>
-            <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $classicoR ?>" name="classico" id="classico" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                <label for="classico" class="labelInput">Tarifa Clássico (%)</label></div>
-            <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $premiumR ?>" name="premium" id="premium" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                <label for="premium" class="labelInput">Tarifa Premium (%)</label></div>
-            <div class="mb-4 inputBox">
-                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $vendaR ?>" name="venda" id="venda" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;" min="1">
-                <label for="venda" class="labelInput">Valor do anúncio (R$)</label></div>
+                <input autocomplete="off" class="border rounded-pill shadow-sm form-control inputUser" type="number" pattern="[0-9]+([,\.][0-9]+)?" step="any" value="<?php echo $margemLucroR ?>" name="margemLucro" id="margemLucro" min="0" required inputmode="numeric" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;" min="1">
+                <label for="margemLucro" class="labelInput">Margem de lucro</label></div>
             <div class="mb-4 inputBox">
                 <button class="btn btn-primary d-block w-100" type="submit" id="submit" style="background: #5850fe;">Calcular</button></div>
                 <!-- <button class="mb-5 btn btn-primary d-block w-100" type="reset" style="background: #5850fe;margin: 5px 0px 0px;">Limpar</button> --><br>
             <section id="resultado">
                 <div class="mb-5 inputBox">
                     <div class="inputUserResultado inputBox">
-                        <p class="smallTitle">Clássico - (Lucro líquido)</p>
+                        <p class="smallTitle">Preço de venda</p>
                         <input class="border rounded-pill shadow-sm form-control mb-4" readonly name="resultadoClassico" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                        <b><label for="resultadoClassico" class="resultadoInput"><?php echo ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'R$'.$totalclassico : "" ?></label></b></div>
+                        <b><label for="resultadoClassico" class="resultadoInput"><?php echo ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'R$'.$precoVenda : "" ?></label></b></div>
                         <div class="inputUserResultado inputBox">
-                        <p class="smallTitle">Premium - (Lucro líquido)</p>
-                        <input class="mb-4 border rounded-pill shadow-sm form-control" readonly name="resultadoPremium" style="border-color: #5850fe;--bs-primary: #5850fe;--bs-primary-rgb: 88,80,254;">
-                        <b><label for="resultadoPremium" class="resultadoInput"><?php echo ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'R$'.$totalpremium : "" ?></label><b></div>
                 </div>
             </section>
         </form>
